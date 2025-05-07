96752ce5159e76230010d45b8a86765aef9e9180
certified454 <fj689669@gmail.com>
2025-05-07 03:52:04 +0100

    made changes to authRouter by adding image

--- src/index.js
+++ src/index.js
@@ -10,6 +10,8 @@ import { connectDB } from "./lib/db.js"
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 app.use(express.json());
 app.use(cors());
