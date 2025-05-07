5f577c99feb46d4ef1f4f65e8ff7f0faeb265281
certified454 <fj689669@gmail.com>
2025-05-07 03:42:23 +0100

    made changes to authRouter by adding image

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import express from "express"; 
 import "dotenv/config";
 import cors from "cors";
+const path = require('path');   
 
 import authRoutes from "./routes/authRoutes.js";
 import { connectDB } from "./lib/db.js"
