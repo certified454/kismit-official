5cf8bf812f4064dbe7737d2218bbc45f96ed4648
certified454 <fj689669@gmail.com>
2025-05-23 13:09:51 +0100

    now modified backend to use multer in uploading files

--- src/index.js
+++ src/index.js
@@ -10,7 +10,6 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json({limit: '100mb'}));
-app.use(express.urlencoded({limit: '100mb', extended: true}));
 app.use(cors());
 app.use("/api/auth", authRoutes);
 app.use("/api/posts", postRoutes);
