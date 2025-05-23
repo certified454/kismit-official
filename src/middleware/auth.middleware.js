b25dde4a40002502ea16c8f3ce9920e36343f530
certified454 <fj689669@gmail.com>
2025-05-23 11:05:00 +0100

    modified authmiddleware

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -4,9 +4,13 @@ import User from '../modules/user.js';
 const protectRoute = async (req, res, next) => {
     try{
         //get token from header
-        const token = req.headers("Authrization").replace("Bearer", "");
-        if (!token) return res.status(401).json({ message: "No token provided" });
-        
+        let token;
+
+        if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')){
+            token = req.headers.authorization.split(' ')[1];
+        } else {
+            return res.status(401).json({message: "No token providede or malformed heardesr"})
+        }
         //verify token
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
         
