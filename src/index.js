97c6144d09cc4b6d63f4b004bc8087b0ca83a30c
certified454 <fj689669@gmail.com>
2025-07-25 03:56:48 -0700

    change application to app in src/index.js

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ app.use(express.json({limit: '100mb'}));
 app.use(cors());
 app.use("/api/auth", authRoutes);
 app.use("/api/post", postRoutes);
-application.use("/api/comment", textcommentRoutes);
+app.use("/api/comment", textcommentRoutes);
 
 app.listen(PORT, () => {
     console.log('Server is running on port 3000');
