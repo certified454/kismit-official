2992e5d8b5114e4065e2e70f1cf4c6791c326044
certified454 <fj689669@gmail.com>
2025-08-11 05:13:22 -0700

    added follow for users

--- src/routes/userProfileRoutes.js
+++ src/routes/userProfileRoutes.js
@@ -34,69 +34,68 @@ router.get('/:userId', protectRoute, async (req, res) => {
     }
 })
 
-// router.post('/:userId/follow', protectRoute, async (req, res) => {
-//     const targetUserObjectId = req.params.userId;
-//     const currentUserObjectId = req.user._id
+router.post('/:userId/follow', protectRoute, async (req, res) => {
+    const targetUserObjectId = req.params.userId;
+    const currentUserObjectId = req.user._id
 
-//     if(targetUserObjectId === currentUserObjectId.toString()) {
-//         console.log('You cannot follow yourself');
-//         return res.status(400).json({ message: "You cannot follow yourself" });
-//     }
+    if(targetUserObjectId === currentUserObjectId.toString()) {
+        console.log('You cannot follow yourself');
+        return res.status(400).json({ message: "You cannot follow yourself" });
+    }
 
-//     try {
-//         const targetUser = await User.findById(targetUserObjectId);
-//         const currentUser = await User.findById(currentUserObjectId)
+    try {
+        const targetUser = await User.findById(targetUserObjectId);
+        const currentUser = await User.findById(currentUserObjectId)
 
-//         if(!targetUser  || !currentUser) {
-//             console.log('User not found')
-//             return res.status(404).json({ message: "User not found" });
-//         };
+        if(!targetUser  || !currentUser) {
+            console.log('User not found')
+            return res.status(404).json({ message: "User not found" });
+        };
 
-//         const followed = currentUser.following.includes(targetUserObjectId);
+        const followed = currentUser.following.includes(targetUserObjectId);
+        
+        let message;
 
-//         let update;
-//         let message;
+        if(followed){
+            await User.findByIdAndUpdate(currentUserObjectId, {
+                $pull: { following: targetUserObjectId },
+                $inc: { followersCount: -1 }
+            }) 
+            await User.findByIdAndUpdate(targetUserObjectId, {
+                $pull: { followers: currentUserObjectId },
+                $inc: { followersCount: -1 }
+            }) 
+            message = 'You Unfollowed this user '
+        } else {
+            await User.findByIdAndUpdate(currentUserObjectId, {
+                $addToSet: { following: targetUserObjectId },
+                $inc: { followersCount: 1 }
+            }) 
+            await User.findByIdAndUpdate(targetUserObjectId, {
+                $addToSet: { followers: currentUserObjectId },
+                $inc: { followersCount: 1 }
+            }) 
+            message = 'You followed this user'
+        }
+        const updatedUser = await User.findByIdAndUpdate(targetUserObjectId)
+        req.app.get('io').emit('new follower', {
+            userId: targetUserObjectId,
+            followerId: currentUserObjectId,
+            followed: !followed
+        });
 
-//         if(followed){
-//             await User.findByIdAndUpdate(currentUserObjectId, {
-//                 $pull: { following: targetUserObjectId },
-//                 $inc: { followersCount: -1 }
-//             }) 
-//             await User.findByIdAndUpdate(targetUserObjectId, {
-//                 $pull: { followers: currentUserObjectId },
-//                 $inc: { followersCount: -1 }
-//             }) 
-//             message = 'You Unfollowed this user '
-//         } else {
-//             await User.findByIdAndUpdate(currentUserObjectId, {
-//                 $addToSet: { following: targetUserObjectId },
-//                 $inc: { followersCount: 1 }
-//             }) 
-//             await User.findByIdAndUpdate(targetUserObjectId, {
-//                 $addToSet: { followers: currentUserObjectId },
-//                 $inc: { followersCount: 1 }
-//             }) 
-//             message = 'You followed this user'
-//         }
-//         const updatedUser = await User.findByIdAndUpdate(targetUserObjectId)
-//         req.app.get('io').emit('new follower', {
-//             userId: targetUserObjectId,
-//             followerId: currentUserObjectId,
-//             followed: !followed
-//         });
-
-//         res.status(200).json({
-//             message,
-//             user: updatedUser,
-//             success: true
-//         })
-//     } catch (error) {
-//         console.error(error, 'Failed')
-//         res.status(500).json({
-//             message: 'Internal server error',
-//             success: false
-//         })
-//     }
-// })
+        res.status(200).json({
+            message,
+            user: updatedUser,
+            success: true
+        })
+    } catch (error) {
+        console.error(error, 'Failed')
+        res.status(500).json({
+            message: 'Internal server error',
+            success: false
+        })
+    }
+})
 
 export default router;
