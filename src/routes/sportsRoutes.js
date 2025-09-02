b276ece89c4df3b9dacafe76d846496f0fdac66c
certified454 <fj689669@gmail.com>
2025-09-02 10:08:49 -0700

    added sports route and implemented upcoming features route

--- src/routes/sportsRoutes.js
+++ src/routes/sportsRoutes.js
@@ -5,20 +5,20 @@ const router = express.Router();
 
 //define a route fetch sports data using sportmonk api
 router.get('/upcoming-fixtures', protectRoute, async (req, res) => {
-    const SPORTMONK_API_TOKEN = process.env.SPORTMONKS_APIS
+    const SPORTMONK_API_TOKEN = process.env.SPORTMONKS_APIS;
+    const baseUrl = 'https://api.sportmonks.com/v3/football/fixtures/upcoming/markets/2';
+    const queryParams = `?api_token=${SPORTMONK_API_TOKEN}&include=league,season`;
+
+
     try {
-        const response = await fetch(`https://api.sportmonks.com/v3/football/fixtures/upcoming/markets/2`, {
-            params: {
-                api_token: SPORTMONK_API_TOKEN,
-                include: 'league,season'
-            }
-        })
+        const response = await fetch(`${baseUrl}${queryParams}`);
         if (!response.ok) {
             console.log('Error fetching upcoming fixtures:', response.statusText);
             return res.status(response.status).json({ message: 'Error fetching upcoming fixtures' });
         };
+        const data = await response.json();
         console.log('Fetched upcoming fixtures successfully');
-        res.status(200).json(await response.json());
+        res.status(200).json(data);
     } catch (error) {
         console.error('Error fetching sports data:', error);
         res.status(500).json({ message: 'Internal server error' });
