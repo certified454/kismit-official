d874119531773ecb62bbfc901871d2bfc13de1dc
certified454 <fj689669@gmail.com>
2025-09-03 08:01:30 -0700

    fixing vote route

--- src/routes/voteRoutes.js
+++ src/routes/voteRoutes.js
@@ -17,28 +17,30 @@ router.post('/register', protectRoute, async (req, res) => {
             console.log("Challenge not found");
             return res.status(404).json({ message: "Challenge not found" });
         }
-        if(user.includes(challenge._id)) {
+        const existingVote = await Vote.findOne({ user, challenge: challenge._id });
+        if(existingVote) {
             console.log("User has already voted on this challenge");
             return res.status(400).json({ message: "User has already voted on this challenge" });
+        } else {
+            const newVote = new Vote({
+                text,
+                user,
+                challenge: challenge._id
+            });
+            await newVote.save();
+            const populatedVote = await Vote.findById(newVote._id).populate('user', 'username avatarUrl').populate('challenge');
+            req.app.get('io').emit('new vote created', {
+                _id: populatedVote._id,
+                user: {
+                    _id: populatedVote.user._id,
+                    username: populatedVote.user.username,
+                    avatarUrl: populatedVote.user.avatarUrl
+                },
+                text: populatedVote.text,
+            })
+            console.log("Vote submitted:", newVote);
+            res.status(201).json({ message: "Vote submitted", vote: newVote, populatedVote });
         }
-        const newVote = new Vote({
-            text,
-            user,
-            challenge: challenge._id
-        });
-        await newVote.save();
-        const populatedVote = await Vote.findById(newVote._id).populate('user', 'username avatarUrl').populate('challenge');
-        req.app.get('io').emit('new vote created', {
-            _id: populatedVote._id,
-            user: {
-                _id: populatedVote.user._id,
-                username: populatedVote.user.username,
-                avatarUrl: populatedVote.user.avatarUrl
-            },
-            text: populatedVote.text,
-        })
-        console.log("Vote submitted:", newVote);
-        res.status(201).json({ message: "Vote submitted", vote: newVote, populatedVote });
     } catch (error) {
         console.error("Error submitting vote:", error);
         res.status(500).json({ message: "Error submitting vote" });
