fcc5dc5448bef1c916cd037c0dc0e5d8377a1506
certified454 <fj689669@gmail.com>
2025-09-03 07:54:13 -0700

    fixing vote route

--- src/routes/voteRoutes.js
+++ src/routes/voteRoutes.js
@@ -5,8 +5,8 @@ import protectRoute from "../middleware/auth.middleware.js";
 const router = express.Router();
 //route to vote on a challenge
 router.post('/register', protectRoute, async (req, res) => {
-    const { text } = req.body
-
+    const { text } = req.body;
+    const user = req.user._id;
     try {
         if (!text) {
             console.log("No text provided");
@@ -17,14 +17,28 @@ router.post('/register', protectRoute, async (req, res) => {
             console.log("Challenge not found");
             return res.status(404).json({ message: "Challenge not found" });
         }
+        if(user.includes(challenge._id)) {
+            console.log("User has already voted on this challenge");
+            return res.status(400).json({ message: "User has already voted on this challenge" });
+        }
         const newVote = new Vote({
             text,
-            user: req.user.id,
+            user,
             challenge: challenge._id
         });
         await newVote.save();
+        const populatedVote = await Vote.findById(newVote._id).populate('user', 'username avatarUrl').populate('challenge');
+        req.app.get('io').emit('new vote created', {
+            _id: populatedVote._id,
+            user: {
+                _id: populatedVote.user._id,
+                username: populatedVote.user.username,
+                avatarUrl: populatedVote.user.avatarUrl
+            },
+            text: populatedVote.text,
+        })
         console.log("Vote submitted:", newVote);
-        res.status(201).json({ message: "Vote submitted", vote: newVote });
+        res.status(201).json({ message: "Vote submitted", vote: newVote, populatedVote });
     } catch (error) {
         console.error("Error submitting vote:", error);
         res.status(500).json({ message: "Error submitting vote" });
