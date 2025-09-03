7f5e8885e6a9cd21d9e9663d930e96163ff778ff
certified454 <fj689669@gmail.com>
2025-09-02 19:14:51 -0700

    added sports route and implemented livescore route

--- src/routes/sportsRoutes.js
+++ src/routes/sportsRoutes.js
@@ -6,10 +6,11 @@ const router = express.Router();
 //define a route fetch sports data using sportmonk api
 router.get('/fixtures', protectRoute, async (req, res) => {
     const SPORTMONK_API_TOKEN = process.env.SPORTMONKS_APIS;
-    const baseUrl = `https://api.sportmonks.com/v3/football/livescores?api_token=${SPORTMONK_API_TOKEN}`;
+    const baseUrl = 'https://api.sportmonks.com/v3/football/livescores';
+    const queryParams = `?api_token=${SPORTMONK_API_TOKEN}`;
 
     try {
-        const response = await fetch(`${baseUrl}`);
+        const response = await fetch(`${baseUrl}${queryParams}`);
         if (!response.ok) {
             const errorText = await response.text();
             console.log('Error fetching upcoming fixtures:', errorText);
