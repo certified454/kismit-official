956e47cf3099c20506a96794206811c9a80f27d3
certified454 <fj689669@gmail.com>
2025-09-21 07:26:34 -0700

    modify create competition endpoint

--- src/routes/competeRoutes.js
+++ src/routes/competeRoutes.js
@@ -1,33 +1,30 @@
 import express from 'express';
 import protectRoute from '../middleware/auth.middleware.js';
-import Team from '../modules/compete.js';
-import Player from '../modules/compete.js';
 import Compete from '../modules/compete.js';
+import User from '../modules/user.js';
 
 const router = express.Router();
 
 router.post('/register', protectRoute, async (req, res) => {
-    const { name, creatorId, targetedUserId } = req.body;
+    const targetedUserObject = req.body.targetUserId;
+    const currentUserObject = req.user._id;
+    const { name } = req.body;
+
+    if (targetedUserObject === currentUserObject.toString()) {
+        console.log('You cannot challenge yourself');
+        return res.status(400).json({ error: 'You cannot challenge yourself' });
+    }
     try {
-        if (!name) {
-            console.log('Name is required');
-            return res.status(400).json({ error: 'Name is required' });
-        };
-        if(!creatorId || !targetedUserId){ 
-            console.log('creatorId and targetedUserId are required');
-            return res.status(400).json({ error: 'creatorId and targetedUserId are required' });
-        }
-    
-        const creator = await User.findById(creatorId);
-        const targetedUser = await User.findById(targetedUserId);
-        if (!creator || !targetedUser) {
-            console.log('creator or targeted user not found');
-            return res.status(404).json({ error: 'creator or targeted user not found' });
+        const targetUser = await User.findById(targetedUserObject);
+        const creator = await User.findById(currentUserObject);
+        if (!targetUser || !creator) {
+            console.log('Target user or creator not found');
+            return res.status(404).json({ error: 'User not found' });
         }
         const newCompete = new Compete({
             name,
             creator: creator._id,
-            targetedUser: targetedUser._id,
+            targetedUser: targetUser._id
         });
         await newCompete.save();
         console.log('Compete created successfully:', newCompete);
@@ -36,6 +33,6 @@ router.post('/register', protectRoute, async (req, res) => {
         console.error('Error creating compete:', error);
         res.status(500).json({ error: 'Internal server error' });
     }
-})
+});
 
 export default router;
