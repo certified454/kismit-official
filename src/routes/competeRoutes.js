16e85d4c1eeeef6cac79b2378c518770f56ef4dd
certified454 <fj689669@gmail.com>
2025-09-21 06:55:31 -0700

    add targeted user to competion

--- src/routes/competeRoutes.js
+++ src/routes/competeRoutes.js
@@ -7,32 +7,30 @@ import Compete from '../modules/compete.js';
 const router = express.Router();
 
 router.post('/register', protectRoute, async (req, res) => {
-    const { name, teams, creatorId, targetedUserId } = req.body;
+    const { name, creatorId, targetedUserId } = req.body;
     try {
         if (!name) {
             console.log('Name is required');
             return res.status(400).json({ error: 'Name is required' });
         };
-        if (!teams || teams.length < 2) {
-            console.log('At least two teams are required');
-            return res.status(400).json({ error: 'At least two teams are required' });
-        };
         if(!creatorId || !targetedUserId){ 
+            console.log('creatorId and targetedUserId are required');
             return res.status(400).json({ error: 'creatorId and targetedUserId are required' });
         }
     
         const creator = await User.findById(creatorId);
         const targetedUser = await User.findById(targetedUserId);
         if (!creator || !targetedUser) {
+            console.log('creator or targeted user not found');
             return res.status(404).json({ error: 'creator or targeted user not found' });
         }
         const newCompete = new Compete({
             name,
-            teams,
             creator: creator._id,
             targetedUser: targetedUser._id,
         });
         await newCompete.save();
+        console.log('Compete created successfully:', newCompete);
         res.status(201).json({ message: 'Compete created successfully', compete: newCompete });
     } catch (error) {
         console.error('Error creating compete:', error);
