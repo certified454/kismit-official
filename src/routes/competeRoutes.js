41be5c2fe74fed5df48cfd9ba4ea41f29dc0de85
certified454 <fj689669@gmail.com>
2025-10-02 03:12:00 -0700

    add competition id to push notification

--- src/routes/competeRoutes.js
+++ src/routes/competeRoutes.js
@@ -78,7 +78,7 @@ router.post('/register', protectRoute, async (req, res) => {
         await newCompete.save();
         
         if (targetUser.expoPushToken) {
-            const acceptLink = `ksm://(challenge)/create-target-team`;
+            const acceptLink = `ksm://(challenge)/create-target-team?=${newCompete._id}`;
             try {
                 await fetch('https://exp.host/--/api/v2/push/send', {
                     method: 'POST',
