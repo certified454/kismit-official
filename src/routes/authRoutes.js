acae0e2fdaedc723b7ec610e085f6b64884eaceb
certified454 <fj689669@gmail.com>
2025-10-06 17:02:03 -0700

    fixing sending email

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -312,9 +312,9 @@ router.post("/resend-code", async (req, res) => {
 
     const transporter = nodemailer.createTransport({
       service: "gmail",
-      port: 465,
-      secure: true,
-      host: "smtp.gmail.com",
+      // port: 465,
+      // secure: true,
+      // host: "smtp.gmail.com",
       auth: {
         user: process.env.EMAIL,
         pass: process.env.EMAIL_PASSWORD,
