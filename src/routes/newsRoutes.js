e3e2f787ebbd5d565ff6704d499b45d165453b26
certified454 <fj689669@gmail.com>
2025-11-13 19:19:55 -0800

    add fetch all news route endpoint

--- src/routes/newsRoutes.js
+++ src/routes/newsRoutes.js
@@ -46,4 +46,65 @@ router.post('/register', protectRoute, async (req, res) => {
         console.error("Error creating news article:", error);
         res.status(500).json({ message: "Server error" });
     }
+});
+
+// get all news by most engaged ( likes + unlikes)
+router.get('/all', protectRoute, async (req, res) => {
+    try {
+        const page = parseInt(req.query.page) || 1;
+        const limit= parseInt(req.query.limit) || 10;
+        const skip = (page - 1) * limit;
+
+        const newsArticles = await News.aggregate(
+            [
+            // Calculate total engagement and sort by it
+                { $addFields: { 
+                    totalEngagement: { $add: [ "$likesCount", "$unlikesCount" ] } },
+                    likeByUser: { 
+                        $in: [req.user._id, { $ifNull: ['$like', []]}]
+                    },
+                    unlikeByUser: { 
+                        $in: [req.user._id, { $ifNull: ['$unlike', []]}]
+                    },
+                   
+                },
+                { $sort:  
+                    { totalEngagement: -1}
+                },
+                { $skip: skip },
+                { $limit: limit },
+                { $lookup: {
+                    from: 'users',
+                    localField: 'users',
+                    foreignField: '_id',
+                    as: 'user'
+                } },
+                { $unwind: '$user' },
+                { $project: {
+                    _id: 1,
+                    description: 1,
+                    pictures: 1,
+                    createdAt: 1,
+                    updatedAt: 1,
+                    user: {
+                        _id: '$user._id',
+                        username: '$user.username',
+                        profilePicture: '$user.profilePicture'
+                    },
+                    likesCount: 1,
+                    unlikesCount: 1
+                }}
+            ]
+        );
+        const totalNewsCount = await News.countDocuments();
+        res.send({
+            newsArticles, 
+            totalNewsCount,
+            currentPage: page,
+            totalPages: Math.ceil(totalNewsCount / limit)
+        })
+    } catch (error) {
+        console.error("Error fetching news articles:", error);
+        res.status(500).json({ message: "Server error" });
+    }
 })
