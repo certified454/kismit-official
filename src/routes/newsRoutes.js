e9320d189c9fe17e395240c9f7810cce68da513b
certified454 <fj689669@gmail.com>
2025-11-25 05:20:51 -0800

    add earnings route

--- src/routes/newsRoutes.js
+++ src/routes/newsRoutes.js
@@ -4,6 +4,7 @@ import News from "../modules/news.js"
 import protectRoute from '../middleware/auth.middleware.js';
 import User from '../modules/user.js';
 import cloudinary from '../lib/cloudinary.js';
+import mongoose from 'mongoose';
 
 const router = express.Router();
 
@@ -55,18 +56,19 @@ router.get('/all', protectRoute, async (req, res) => {
         const limit= parseInt(req.query.limit) || 10;
         const skip = (page - 1) * limit;
 
+        const userObjectId = mongoose.Types.ObjectId(req.user._id);
         const newsArticles = await News.aggregate(
             [
             // Calculate total engagement and sort by it
-                { $addFields: { 
-                    totalEngagement: { $add: [ "$likesCount", "$unlikesCount" ] } },
-                    likeByUser: { 
-                        $in: [req.user._id, { $ifNull: ['$like', []]}]
-                    },
-                    unlikeByUser: { 
-                        $in: [req.user._id, { $ifNull: ['$unlike', []]}]
-                    },
-                   
+                { $addFields: {
+                        totalEngagement: { $add: [ '$likesCount', '$unlikesCount' ]},
+                        likedByUser: {
+                            $in: [userObjectId, { $ifNull: [ '$likedBy', [] ]}]
+                        },
+                        unlikedByUser: {
+                            $in: [userObjectId, { $ifNull: [ '$unlikedBy', [] ]}]
+                        }
+                    }
                 },
                 { $sort:  
                     { totalEngagement: -1}
@@ -75,7 +77,7 @@ router.get('/all', protectRoute, async (req, res) => {
                 { $limit: limit },
                 { $lookup: {
                     from: 'users',
-                    localField: 'users',
+                    localField: 'user',
                     foreignField: '_id',
                     as: 'user'
                 } },
@@ -92,7 +94,9 @@ router.get('/all', protectRoute, async (req, res) => {
                         profilePicture: '$user.profilePicture'
                     },
                     likesCount: 1,
-                    unlikesCount: 1
+                    unlikesCount: 1,
+                    likedByUser: 1,
+                    unlikedByUser: 1
                 }}
             ]
         );
@@ -107,4 +111,43 @@ router.get('/all', protectRoute, async (req, res) => {
         console.error("Error fetching news articles:", error);
         res.status(500).json({ message: "Server error" });
     }
-})
+});
+
+router.get('/earnings', protectRoute, async (req, res) => {
+    // lets get the article id and check how many likes and unlikes it has
+    const userId = req.user._id;
+    const newsId = req.query.newsId;
+    try {
+        if (!newsId) {
+            console.log('News article ID is required.');
+            return  res.status(400).json({ message: "News article ID is required." });
+        };
+        if (!mongoose.Types.ObjectId.isValid(newsId)) {
+            console.log('Invalid news article ID.');
+            return res.status(400).json({ message: "Invalid news article ID." });
+        };
+        
+        const newsArticle = await News.findById( newsId);
+        if (newsArticle.user.toString() !== userId.toString()) {
+            return res.status(403).json({ message: "You are not authorized to view earnings for this news article." });
+        };
+        if (!newsArticle) {
+            console.log('News article not found.');
+            return res.status(404).json({ message: "News article not found." });
+        };
+        if (newsArticle.likesCount === 0 && newsArticle.unlikesCount === 0) {
+            console.log('No likes or unlikes for this news article.');
+            return res.status(200).json({ totalPoints: 0, totalEarnings: 0 });
+        } 
+        
+        //calculate earnings
+        const totalPoints = newsArticle.likesCount * 2 + newsArticle.unlikesCount * 1;
+        const totalEarnings = (totalPoints * 0.05).toFixed(2);
+
+        console.log(`Total points: ${totalPoints}, Total earnings: $${totalEarnings}`);
+        return res.status(200).json({ totalPoints, totalEarnings });
+    } catch (error) {
+        console.error("Error calculating earnings for news article:", error);
+        res.status(500).json({ message: "Server error" });
+    }
+});
